300c890869be0ffbba34f23d868c191c26c2eb4f
saltnlight <arongflorence@yahoo.com>
2022-04-18 09:06:50 -0700

    advanced task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *  * main - prints exactly "and that piece of art is useful"
+ *   * - Dora Korpar, 2015-10-19\",
+ *    * followed by a new line, to the standard error.
+ *     * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
+	return (1);
+}
