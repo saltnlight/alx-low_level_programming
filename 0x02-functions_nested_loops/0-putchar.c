1203c96a05ad9950a1731737b754724a30941023
saltnlight <arongflorence@yahoo.com>
2022-04-18 11:49:08 -0700

    C funcs and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main -  prints Holberton, followed by a new line
+ *  * Return: 0 (Success)
+ **/
+int main(void){
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
