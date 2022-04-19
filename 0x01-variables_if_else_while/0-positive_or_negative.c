683306901577d057ee0c7f57044d9cfd37bcd779
saltnlight <arongflorence@yahoo.com>
2022-04-19 01:45:31 -0700

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,12 @@
 /**
  *  * main - assigns a random number to int n everytime
  *  * it executes, and prints it
- *  * Return: Always 0 
+ *  * Return: Always 0
  **/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0. program will assign a random number to the variable n each time it is executed
+1. like task0 but prints only the last digit of the number
