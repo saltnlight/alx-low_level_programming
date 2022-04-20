afffd63354bad91c29520cfc7d922c3945299698
saltnlight <arongflorence@yahoo.com>
2022-04-20 03:08:24 -0700

    task4 and task3 correction

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+**/
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
